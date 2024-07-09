d0c99a9f8efbec7163240c7f4dba9c75493fc0ae
sanjaymuthusamy2004 <sanjay.m192004@gmail.com>
2024-07-09 22:12:08 +0530

    first commit

---
+++ src/api-helpers/api-helpers.js
@@ -0,0 +1,152 @@
+import axios from "axios";
+
+const BASE_URL = 'https://fullstackbackend-1.onrender.com';
+
+export const getAllMovies = async () => {
+  const res = await axios.get(`${BASE_URL}/movie`).catch((err) => console.log(err));
+
+  if (res.status !== 200) {
+    return console.log("No Data");
+  }
+
+  const data = await res.data;
+  return data;
+};
+
+export const sendUserAuthRequest = async (data, signup) => {
+  const res = await axios
+    .post(`${BASE_URL}/user/${signup ? "signup" : "login"}`, {
+      name: signup ? data.name : "",
+      email: data.email,
+      password: data.password,
+    })
+    .catch((err) => console.log(err));
+
+  if (res.status !== 200 && res.status !== 201) {
+    console.log("Unexpected Error Occurred");
+  }
+
+  const resData = await res.data;
+  return resData;
+};
+
+export const sendAdminAuthRequest = async (data) => {
+  const res = await axios
+    .post(`${BASE_URL}/admin/login`, {
+      email: data.email,
+      password: data.password,
+    })
+    .catch((err) => console.log(err));
+
+  if (res.status !== 200) {
+    return console.log("Unexpected Error");
+  }
+
+  const resData = await res.data;
+  return resData;
+};
+
+export const getMovieDetails = async (id) => {
+  const res = await axios.get(`${BASE_URL}/movie/${id}`).catch((err) => console.log(err));
+  if (res.status !== 200) {
+    return console.log("Unexpected Error");
+  }
+  const resData = await res.data;
+  return resData;
+};
+
+export const newBooking = async (data) => {
+  const res = await axios
+    .post(`${BASE_URL}/booking`, {
+      movie: data.movie,
+      seatNumber: data.seatNumber,
+      date: data.date,
+      user: localStorage.getItem("userId"),
+    })
+    .catch((err) => console.log(err));
+
+  if (res.status !== 201) {
+    return console.log("Unexpected Error");
+  }
+  const resData = await res.data;
+  return resData;
+};
+
+export const getUserBooking = async () => {
+  const id = localStorage.getItem("userId");
+  const res = await axios
+    .get(`${BASE_URL}/user/bookings/${id}`)
+    .catch((err) => console.log(err));
+
+  if (res.status !== 200) {
+    return console.log("Unexpected Error");
+  }
+  const resData = await res.data;
+  return resData;
+};
+
+export const deleteBooking = async (id) => {
+  const res = await axios
+    .delete(`${BASE_URL}/booking/${id}`)
+    .catch((err) => console.log(err));
+
+  if (res.status !== 200) {
+    return console.log("Unexpected Error");
+  }
+
+  const resData = await res.data;
+  return resData;
+};
+
+export const getUserDetails = async () => {
+  const id = localStorage.getItem("userId");
+  const res = await axios.get(`${BASE_URL}/user/${id}`).catch((err) => console.log(err));
+  if (res.status !== 200) {
+    return console.log("Unexpected Error");
+  }
+  const resData = await res.data;
+  return resData;
+};
+
+export const addMovie = async (data) => {
+  const res = await axios
+    .post(
+      `${BASE_URL}/movie`,
+      {
+        title: data.title,
+        description: data.description,
+        releaseDate: data.releaseDate,
+        posterUrl: data.posterUrl,
+        featured: data.featured,
+        actors: data.actors,
+        admin: localStorage.getItem("adminId"),
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    )
+    .catch((err) => console.log(err));
+
+  if (res.status !== 201) {
+    return console.log("Unexpected Error Occurred");
+  }
+
+  const resData = await res.data;
+  return resData;
+};
+
+export const getAdminById = async () => {
+  const adminId = localStorage.getItem("adminId");
+  const res = await axios
+    .get(`${BASE_URL}/admin/${adminId}`)
+    .catch((err) => console.log(err));
+
+  if (res.status !== 200) {
+    return console.log("Unexpected Error Occurred");
+  }
+
+  const resData = await res.data;
+  return resData;
+};
